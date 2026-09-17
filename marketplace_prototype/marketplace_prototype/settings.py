75dc9bc569c93700d5a062da5af1d23370e83ea7
Chinomso-Onwubiko <chinomso.onwubiko@pau.edu.ng>
2026-09-17 18:10:27 +0100

    Configure WhiteNoise for static files

--- marketplace_prototype/marketplace_prototype/settings.py
+++ marketplace_prototype/marketplace_prototype/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Enable WhiteNoise compression and caching
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
